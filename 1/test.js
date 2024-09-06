bdb9b3125143fea29b5a1855ab2a2578b7837ab6
skyptdr <165387193+skyptdr@users.noreply.github.com>
2024-09-06 17:12:36 +0000

    custom config done

--- 1/test.js
+++ 1/test.js
@@ -1,1 +1,16 @@
-console.log("akash")
+const customerId =14452635
+let  firstName = "akash"
+var lastName ="patidar"
+
+accountCity= "jaipur"
+// customerId=232323
+
+firstName="akakka"
+lastName="sir"
+accountCity="kolapur"
+
+console.log(customerId);
+console.table([accountCity,firstName,lastName,customerId]);
+
+;
+
